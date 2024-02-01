fb5f1ae6826d8ad2a975cf1e8b158f10def7e7cf
jayonkeys <118300862+jayonkeys@users.noreply.github.com>
2024-02-01 17:41:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,13 @@ let navlist = document.querySelector('.navlist');
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navlist.classList.toggle('open');
-}
+};
+
+const sr = ScrollReveal ({
+    distance: '65px',
+    duration: 2600,
+    delay: 450,
+    reset: true
+});
+
+sr.reveal('.hero-text',{delay: 200, origin: 'top'});
